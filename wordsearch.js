38afc273b183f1e8d0f7bdf8b580089b6ddaeda9
shivanix <shvni.here@gmail.com>
2021-07-01 03:07:08 +0000

    made a few changes for code to return the matched word horizontally & vertically

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,30 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) {
 
-module.exports = wordSearch
+      return true;
+    }
+  }
+
+  const rows = letters.length;
+  const columns = letters[0].length;
+
+  for (let i = 0; i < columns; i++) {
+    let currentString = "";
+    for (let j = 0; j < rows; j++) {
+      const rowIndex = j;
+      const columnIndex = i;
+      currentString += letters[rowIndex][columnIndex];
+
+    }
+    if (currentString === word) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
+
+module.exports = wordSearch;
